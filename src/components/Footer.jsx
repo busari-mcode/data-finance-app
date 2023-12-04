3fa709a796f3dabfcd49b49e8b5e9d46a0b0beb5
Busari Moruf <busarimoruf20@gmail.com>
2023-12-04 23:57:45 +0100

    new changes

---
+++ src/components/Footer.jsx
@@ -0,0 +1,28 @@
+import React from 'react';
+import {
+    FaDribbbleSquare,
+    FaFacebookSquare,
+    FaGithubSquare,
+    FaInstagram,
+    FaTwitterSquare
+} from 'react-icons/fa'
+
+const Footer = () => {
+  return (
+    <div className='max-w-[1240px] mx-auto py-16 px-4 grid lg:grid-cols-3 gap-8 text-gray-300'>
+        <div>
+        <h1 className="w-full text-3xl font-bold text-[#00df9a]">REACT.</h1>
+        <p className='py-4'>Lorem ipsum dolor sit amet consectetur adipisicing elit. Velit eius nesciunt ullam veniam beatae animi porro, dolorem ad sit nam!</p>
+        <div className='flex justify-between md:w-[75%] my-6'>
+            <FaFacebookSquare size={30} /> 
+            <FaInstagram size={30} /> 
+            <FaTwitterSquare size={30} /> 
+            <FaGithubSquare size={30} /> 
+            <FaDribbbleSquare size={30} /> 
+        </div>
+        </div>
+    </div>
+  )
+}
+
+export default Footer
