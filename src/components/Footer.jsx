d17d13a69b6ce64ce04375e5c5e350527b886e97
Busari Moruf <busarimoruf20@gmail.com>
2023-12-07 23:03:10 +0100

    new changes

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -21,7 +21,7 @@ const Footer = () => {
             <FaDribbbleSquare size={30} /> 
         </div>
         </div>
-        <div className='lg:col-span-3 flex justify-between'>
+        <div className='lg:col-span-2 flex justify-between mt-6'>
           <div>
             <h6 className='font-medium text-gray-400'>Solutions</h6>
             <ul>
@@ -29,6 +29,15 @@ const Footer = () => {
               <li className='py-2 text-sm'>Marketing</li>
               <li className='py-2 text-sm'>Commerce</li>
               <li className='py-2 text-sm'>Insights</li>
+            </ul>
+          </div>
+          <div>
+            <h6 className='font-medium text-gray-400'>Support</h6>
+            <ul>
+              <li className='py-2 text-sm'>Pricing</li>
+              <li className='py-2 text-sm'>Documentation</li>
+              <li className='py-2 text-sm'>Guides</li>
+              <li className='py-2 text-sm'>API Status</li>
             </ul>
           </div>
           <div>
